50f95b4b5f5e0825a20932b343cef8c1f127815b
john.rivera <john.rivera@ganesuperservicios.co>
2025-08-13 10:55:54 -0500

    Se escalo proyecto individualizando las páginas y creando archivos de autenticación

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import { useEffect } from 'react';
-import { Chat } from './components/layout';
-import {
-  Alert,
-  InputField,
-  MessageBubble,
-  MessageText,
-  SendButton,
-} from './components/ui';
-import { useChat } from './hooks';
+import { ChatPage } from '@/pages';
 
 function App() {
-  const {
-    messages,
-    form,
-    loading,
-    error,
-    handleSubmit,
-    handleCloseAlert,
-    handleChange,
-  } = useChat();
-
   useEffect(() => {
     // Detecta la preferencia del usuario
     const userPrefersDark = window.matchMedia(
@@ -35,24 +17,9 @@ function App() {
   }, []);
 
   return (
-    <Chat>
-      <Chat.Body>
-        {messages.map((message, i) =>
-          message.type === 'question' ? (
-            <MessageBubble key={i} message={message.text} />
-          ) : (
-            <MessageText key={i} message={message.text} />
-          )
-        )}
-      </Chat.Body>
-      <Chat.Footer onSubmit={handleSubmit}>
-        {error && (
-          <Alert variant="danger" message={error} onClose={handleCloseAlert} />
-        )}
-        <InputField value={form.question} onChange={handleChange} />
-        <SendButton disabled={!form.question || loading} loading={loading} />
-      </Chat.Footer>
-    </Chat>
+    <>
+      <ChatPage />
+    </>
   );
 }
 
---
+++ src/pages/ChatPage.jsx
@@ -0,0 +1,44 @@
+import { Chat } from '@/components/layout';
+import {
+  Alert,
+  InputField,
+  MessageBubble,
+  MessageText,
+  SendButton,
+} from '@/components/ui';
+import { useChat } from '@/hooks';
+
+const ChatPage = () => {
+  const {
+    messages,
+    form,
+    loading,
+    error,
+    handleSubmit,
+    handleCloseAlert,
+    handleChange,
+  } = useChat();
+
+  return (
+    <Chat>
+      <Chat.Body>
+        {messages.map((message, i) =>
+          message.type === 'question' ? (
+            <MessageBubble key={i} message={message.text} />
+          ) : (
+            <MessageText key={i} message={message.text} />
+          )
+        )}
+      </Chat.Body>
+      <Chat.Footer onSubmit={handleSubmit}>
+        {error && (
+          <Alert variant="danger" message={error} onClose={handleCloseAlert} />
+        )}
+        <InputField value={form.question} onChange={handleChange} />
+        <SendButton disabled={!form.question || loading} loading={loading} />
+      </Chat.Footer>
+    </Chat>
+  );
+};
+
+export default ChatPage;
