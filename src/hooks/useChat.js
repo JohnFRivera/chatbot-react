33bff95c556349246e09bc862e9d6307b48b7667
john.rivera <john.rivera@ganesuperservicios.co>
2025-08-14 14:00:56 -0500

    Se realizaron modificaciones en el modulo del chat

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -5,17 +5,27 @@ import { useState } from 'react';
  *
  * @returns {{
  * messages: Array<{ type: 'question' | 'answer', text: string }>,
+ * reports: Array<{ id: number, value: string }>,
  * form: { question: string },
+ * inform: number | null,
  * loading: boolean,
  * error: string | null,
  * handleSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>,
- * handleCloseAlert: () => void,
- * handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+ * handleAlertClose: () => void,
+ * handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+ * handleInformChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+ * handleChatDelete: () => void
  * }} Un objeto con los estados y las funciones para el componente de chat.
  */
 const useChat = () => {
   const [messages, setMessages] = useState([]);
+  const reports = [
+    { id: 1, value: 'Información desactualizada' },
+    { id: 2, value: 'Modalidades de juego erroneas' },
+    { id: 3, value: 'Sin información' },
+  ];
   const [form, setForm] = useState({ question: '' });
+  const [inform, setInform] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const chatContainer = document.getElementById('chat-container');
@@ -71,7 +81,7 @@ const useChat = () => {
   /**
    * Un manejador de eventos para cerrar o ocultar una alerta.
    */
-  const handleCloseAlert = () => setError(null);
+  const handleAlertClose = () => setError(null);
 
   /**
    * Un manejador de eventos genérico para actualizar el estado de un formulario.
@@ -83,14 +93,36 @@ const useChat = () => {
     setForm((prev) => ({ ...prev, [name]: value }));
   };
 
+  /**
+   * Un manejador de eventos genérico para actualizar el estado de un informe.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} e - El evento de cambio del elemento del formulario.
+   */
+  const handleInformChange = (e) => {
+    setInform(e.target.value);
+  };
+
+  /**
+   * Un manejador de eventos para eliminar chat actual.
+   */
+  const handleChatDelete = () => {
+    setMessages([]);
+    setForm({ question: '' });
+    setError(null);
+  };
+
   return {
     messages,
+    reports,
     form,
+    inform,
     loading,
     error,
     handleSubmit,
-    handleCloseAlert,
+    handleAlertClose,
     handleChange,
+    handleInformChange,
+    handleChatDelete,
   };
 };
 
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
 import {
   ActionsMenu,
   Alert,
+  Button,
   Chat,
   InputField,
   MessageBubble,
   MessageText,
+  Modal,
+  Radio,
   SendButton,
   Spinner,
 } from '@/components/chat';
-import { useChat, useOutsideClick } from '@/hooks';
+import { useChat, useOutsideClick, useToggle } from '@/hooks';
 
 /**
  * Un componente de página principal para una interfaz de chat.
@@ -17,59 +19,139 @@ import { useChat, useOutsideClick } from '@/hooks';
  * @returns {React.JSX.Element} El componente de página del chat renderizado.
  */
 const ChatPage = () => {
-  const [show, setShow] = useState(false);
   const {
     messages,
+    reports,
     form,
+    inform,
     loading,
     error,
     handleSubmit,
-    handleCloseAlert,
+    handleAlertClose,
     handleChange,
+    handleInformChange,
+    handleChatDelete,
   } = useChat();
-  const ref = useOutsideClick(() => setShow(false));
+  const { toggles, toggle, open, close } = useToggle([
+    'showActionsMenu',
+    'showReportModal',
+    'showDeleteModal',
+  ]);
+  const ref = useOutsideClick(() => close('showActionsMenu'));
 
   return (
-    <Chat>
-      <Chat.Header>
-        <p className="text-body-emphasis rounded-4 fs-5 lh-1 py-2 px-3 mb-0">
-          SuperGIROS
-        </p>
-        <div>
-          <ActionsMenu ref={ref} show={show} onToggle={() => setShow(!show)}>
-            <ActionsMenu.Button onClick={() => setShow(false)}>
-              <i className="bi bi-archive me-2"></i>
-              Archivar
-            </ActionsMenu.Button>
-            <ActionsMenu.Button onClick={() => setShow(false)}>
-              <i className="bi bi-flag me-2"></i>
-              Informar
-            </ActionsMenu.Button>
-            <ActionsMenu.Button variant="delete" onClick={() => setShow(false)}>
-              <i className="bi bi-trash me-2"></i>
-              Eliminar
-            </ActionsMenu.Button>
-          </ActionsMenu>
-        </div>
-      </Chat.Header>
-      <Chat.Body>
-        {messages.map((message, i) =>
-          message.type === 'question' ? (
-            <MessageBubble key={i} message={message.text} />
-          ) : (
-            <MessageText key={i} message={message.text} />
-          )
-        )}
-        {loading && <Spinner />}
-        {error && (
-          <Alert variant="danger" message={error} onClose={handleCloseAlert} />
-        )}
-      </Chat.Body>
-      <Chat.Footer onSubmit={handleSubmit}>
-        <InputField value={form.question} onChange={handleChange} />
-        <SendButton disabled={!form.question || loading} loading={loading} />
-      </Chat.Footer>
-    </Chat>
+    <>
+      <Chat>
+        <Chat.Header>
+          <p className="text-body-emphasis rounded-4 fs-5 lh-1 py-2 px-3 mb-0">
+            SuperGIA
+          </p>
+          <div>
+            <ActionsMenu
+              ref={ref}
+              show={toggles.showActionsMenu}
+              onToggle={() => toggle('showActionsMenu')}
+            >
+              <ActionsMenu.Button
+                onClick={() => {
+                  close('showActionsMenu');
+                  open('showReportModal');
+                }}
+              >
+                <i className="bi bi-flag me-2"></i>
+                Informar
+              </ActionsMenu.Button>
+              <ActionsMenu.Button
+                variant="delete"
+                disabled={!messages.length}
+                onClick={() => {
+                  close('showActionsMenu');
+                  open('showDeleteModal');
+                }}
+              >
+                <i className="bi bi-trash me-2"></i>
+                Eliminar
+              </ActionsMenu.Button>
+            </ActionsMenu>
+          </div>
+        </Chat.Header>
+        <Chat.Body>
+          {messages.map((message, i) =>
+            message.type === 'question' ? (
+              <MessageBubble key={i} message={message.text} />
+            ) : (
+              <MessageText key={i} message={message.text} />
+            )
+          )}
+          {loading && <Spinner />}
+          {error && (
+            <Alert
+              variant="danger"
+              message={error}
+              onClose={handleAlertClose}
+            />
+          )}
+        </Chat.Body>
+        <Chat.Footer onSubmit={handleSubmit}>
+          <InputField value={form.question} onChange={handleChange} />
+          <SendButton disabled={!form.question || loading} loading={loading} />
+        </Chat.Footer>
+      </Chat>
+      <Modal show={toggles.showReportModal}>
+        <Modal.Header
+          title="Informar de una conversación"
+          onClose={() => close('showReportModal')}
+        />
+        <Modal.Body>
+          <form className="vstack gap-2">
+            {reports.map((report, i) => (
+              <Radio
+                key={i}
+                id={'report' + (i + 1)}
+                name="reportItems"
+                label={report.value}
+                value={report.id}
+                onChange={handleInformChange}
+              />
+            ))}
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="send"
+            disabled={!inform}
+            onClick={() => close('showReportModal')}
+          >
+            Siguiente
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={toggles.showDeleteModal}>
+        <Modal.Header title="¿Eliminar chat?" />
+        <Modal.Body>
+          <p className="mb-0">
+            Esto eliminará <b>este chat</b>.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="outline-action"
+            onClick={() => close('showDeleteModal')}
+          >
+            Cancelar
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => {
+              handleChatDelete();
+              close('showDeleteModal');
+            }}
+          >
+            Eliminar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
